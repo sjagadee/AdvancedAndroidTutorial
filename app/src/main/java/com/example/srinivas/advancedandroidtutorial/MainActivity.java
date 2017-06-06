181aa63e462281faaa0216ce24646511b144deeb
sjagadee <sjagadee@hawk.iit.edu>
2017-06-06 03:47:23 -0500

    Update to Use Action Bar (Like Instagram)

--- app/src/main/java/com/example/srinivas/advancedandroidtutorial/MainActivity.java
+++ app/src/main/java/com/example/srinivas/advancedandroidtutorial/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.srinivas.advancedandroidtutorial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lvTopList;
 
-    String[] listOfString = {"Fragments" };
+    String[] listOfString = {"Fragments", "Action Bar" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfString);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listOfString);
 
         lvTopList = (ListView) findViewById(R.id.lvTopList);
         lvTopList.setAdapter(itemsAdapter);
@@ -31,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         lvTopList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(TAG, "onItemClick: the " + position + " for fragment");
                 switch (position) {
                     case 0:
                         Intent intent = new Intent(getApplicationContext(), MyFragmentActivity.class);
